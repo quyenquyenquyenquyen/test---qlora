b04d398f45c03188489763369e82ade77ac93ae6
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-04-24 07:20:52 +0700

    Update 01_train_t5_data.sh

--- 01_train_t5_data.sh
+++ 01_train_t5_data.sh
@@ -9,12 +9,13 @@ CUDA_VISIBLE_DEVICES=0 python run_gen.py --do_train --do_eval --do_eval_bleu \
         --task refine --sub_task small --model_type codet5 --data_num -1 \
         --num_train_epochs 3 \
         --warmup_steps 500 \
-        --learning_rate 5e-5 --patience 3 --beam_size 5 \
-        --gradient_accumulation_steps 1 \
+        --learning_rate 1e-5 --patience 3 --beam_size 5 \
+        --max_grad_norm 0.3 \
+        --gradient_accumulation_steps 2 \
         --tokenizer_name=Salesforce/codet5-base \
         --model_name_or_path=Salesforce/codet5-base \
         --bits 4 --double_quant --quant_type nf4 \
-        --bf16 \
+        --fp16 \
         --data_dir "/kaggle/input/daataa10/task2_data/t5_data/codet5_format_data" \
         --cache_path ./model/code2review_t5_data_task2/cache/ \
         --output_dir ./model/code2review_t5_data_task2/outputs/ \
